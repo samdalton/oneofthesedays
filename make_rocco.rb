812dd82c09a5ee74779bb162392f64c6f42ff780
Sam Dalton <mail@samdalton.co.nz>
2011-04-06 16:17:05 +1200

    Corrected bug in make_rocco so output files are correctly prepended with jekyll yaml

--- make_rocco.rb
+++ make_rocco.rb
@@ -13,7 +13,7 @@ Dir.foreach('_rocco') do |file|
         %x{ mv #{output_dir}/#{input_dir}/#{new_file} #{output_dir} }
     
         # add desired header to new post
-        %x{ echo "---\nlayout: post\n---"|cat - #{output_dir}/#{new_file} > /tmp/out && mv /tmp/out #{output_dir}/#{file}  }
+        %x{ echo "---\nlayout: post\n---"|cat - #{output_dir}/#{new_file} > /tmp/out && mv /tmp/out #{output_dir}/#{new_file}  }
 
         # clean up by removing this unwanted dir
         %x{ rm -rf #{output_dir}/#{input_dir} }
