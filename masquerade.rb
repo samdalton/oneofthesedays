26b5bbc873dc333150d15981b2032a5a28f6e44f
Sam Dalton <mail@samdalton.co.nz>
2011-04-06 17:20:32 +1200

    removed routes to test static asset serving

--- masquerade.rb
+++ masquerade.rb
@@ -1,25 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 
-set :public, Proc.new { File.join(root, "_site") }
+set :public, '_site' 
 set :static, true
 
 # This before filter ensures that your pages are only ever served 
 # once (per deploy) by Sinatra, and then by Varnish after that
 before do
   response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
-end
-
-get '/' do
-    send_file( File.join(settings.public, 'index.html') )
-end
-
-get '/favicon.ico' do
-    # return favicon, eventually
-end
-
-get '/*' do
-    send_file( File.join(settings.public, params[:splat], 'index.html') )
 end
 
 
